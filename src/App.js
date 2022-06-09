7192f1714bc2264849180bd28cf36ec15d153bfb
Roy Anger <roy@royanger.com>
2022-06-08 22:09:23 -0400

    Commited changes from discord chat for reference

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
 import './App.css';
-import './css/navbar.css'
-import './css/hero.css'
-import './css/card.css'
 import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero'
-import { Card } from './components/Card'
+import { Hero } from './components/Hero';
+import { Card } from './components/Card';
+
+let mockData;
+
+mockData = [
+   {
+      id: '001',
+      title: 'Life Lessons with Katie Zaferes',
+      person: 'Katie Zaferes',
+      country: 'USA',
+      price: 136,
+      rating: '5.0',
+      reviewCount: 6,
+      image: 'katie-zaferes.png',
+   },
+   {
+      id: '002',
+      title: 'Sport chat with Thane Reynolds',
+      person: 'Thane Reynolds',
+      country: 'Canada',
+      price: 256,
+      rating: '4.0',
+      reviewCount: 9,
+      image: 'katie-zaferes-2.png',
+   },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <main className="main">
-        <Hero />
-        <div className="slider">
-          <Card 
-          img="/images/katie-zaferes.png"
-          rating={5.0} 
-          reviewCount={6} 
-          country="USA" 
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-           />
-        </div>
-      </main>
-    </div>
-  );
+   return (
+      <div className='App'>
+         <Navbar />
+         <main className='main'>
+            <Hero />
+            <div className='slider'>
+               <ul>
+                  {mockData.map((card) => {
+                     return (
+                        <Card
+                           key={card.id}
+                           img={card.image}
+                           person={card.person}
+                           rating={card.rating}
+                           reviewCount={card.reviewCount}
+                           country={card.country}
+                           title={card.title}
+                           price={card.price}
+                        />
+                     );
+                  })}
+               </ul>
+            </div>
+         </main>
+      </div>
+   );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,47 @@
-import star from '../images/star.png'
-import person from '../images/katie-zaferes.png'
+import * as React from 'react';
+import star from '../images/star.png';
 
-export function Card(props){
-  return(
-    <div className="card">
-      <div className="card--image-div">
-        <span className="card--context">SOLD OUT</span>
-        <img src= {props.img} alt="Katie Zaferes" className="card--focusimage" />
+export function Card({
+   title,
+   person,
+   img,
+   country,
+   rating,
+   price,
+   reviewCount,
+}) {
+   const [myVar, setMyVar] = React.useState('');
+   const [cssClasses, setCSSClasses] = React.useState('');
+
+   function handleClick() {
+      setMyVar('hello Thane');
+      setCSSClasses(cssClasses === 'card-clicked' ? '' : 'card-clicked');
+   }
+
+   return (
+      <div className={`card ${cssClasses}`} onClick={handleClick}>
+         <div className='card--image-div'>
+            <span className='card--context'>SOLD OUT</span>
+            <img
+               src={`/images/${img}`}
+               alt={person}
+               className='card--focusimage'
+            />
+         </div>
+         <div className='card--textcontent'>
+            <div className='card--ratingandlocation'>
+               <img src={star} alt='A star' className='card--star' />
+               <span className='card--ratingtext'>{rating}</span>
+               <span className='card--people-location'>
+                  ({reviewCount}) &#183; {country}{' '}
+               </span>
+            </div>
+            <p className='card--eventdescription'>{title}</p>
+            <p className='card--cost'>
+               <span className='card--from'>From ${price}</span> / person
+            </p>
+         </div>
+         <h2>{myVar}</h2>
       </div>
-      <div className="card--textcontent">
-        <div className="card--ratingandlocation">
-          <img src= { star } alt="A star" className="card--star" />
-          <span className="card--ratingtext">{props.rating}</span>
-          <span className="card--people-location">({props.reviewCount}) &#183; {props.country} </span>
-        </div>
-        <p className="card--eventdescription">{props.title}</p>
-        <p className="card--cost"><span className="card--from">From ${props.price}</span> / person</p>
-      </div>
-    </div>
-  )
+   );
 }
